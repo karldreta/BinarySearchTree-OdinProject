f1dbecac6e1a51bdfbd00e732ffafb1ef2c5c30f
Karl James <karldreta@gmail.com>
2024-12-14 13:48:01 +0800

    Add buildTree

--- script.js
+++ script.js
@@ -9,22 +9,43 @@ class Node {
 
 class Tree {
     constructor(array) {
-        this.root = buildTree(array);
+        this.root = buildTree(array, 0, array.length - 1);
     }
 }
 
-function buildTree(array) {
-    let mid = array[Math.floor(array.length / 2)];
-    let left = array.slice(0, mid - 1);
-    let right = array.slice(mid, array.length);
+function buildTree(array, start, end) {
+    
+    // We need a base case.
+    // We need to call this function recursively and divide,
+    if (start > end) return null;
+    const mid = Math.floor((start + end) / 2);
+    const node = new Node(array[mid]);
+    node.left = buildTree(array, start, mid - 1);
+    node.right = buildTree(array, mid + 1, end);
 
-    return mid;
+    return node;
 }
 
-const sampleArr = [1, 2, 3];
-const test = new Node();
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+
+const sampleArr = [1, 2, 3, 4, 5];
 const tree = new Tree(sampleArr);
-console.log(tree.root);
+prettyPrint(tree.root);
+
+    
+// let left = array.slice(0, mid - 1);
+// let right = array.slice(mid, array.length);
 
 
 
