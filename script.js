34f4caa65e9c0b7ec118ab78c648955f2ecb5a3c
Karl James <karldreta@gmail.com>
2024-12-15 09:40:00 +0800

    Add duplicate removal and sorting method

--- script.js
+++ script.js
@@ -13,8 +13,18 @@ class Tree {
     }
 }
 
+// We need to remove the duplicates first
+function removeDuplicatesAndSort(arr) {
+const newArray = arr.filter((item,
+index) => arr.indexOf(item) === index);
+return newArray.sort(function (a, b) {
+  return a - b;
+})
+}
+
 function buildTree(array, start, end) {
-    
+    removeDuplicatesAndSort(array) // But this will also be called in succeeding calls (during the recursion) which will take unnecessary computing power. We'll fix this later.
+
     // We need a base case.
     // We need to call this function recursively and divide,
     if (start > end) return null;
@@ -39,7 +49,7 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
     }
   };
 
-const sampleArr = [1, 2, 3, 4, 5];
+const sampleArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const tree = new Tree(sampleArr);
 prettyPrint(tree.root);
 
